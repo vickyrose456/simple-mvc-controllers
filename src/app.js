6ca7b6b0ebec10a15d20a3ce0d216404c7831528
Austin Willoughby <arwigm@rit.edu>
2022-02-19 13:21:29 -0500

    Removed comments

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-/* Now that we are using Express.js (https://expressjs.com/) our server is now abstracted
-   into an "application" or "app". This app is an encapsulation of all the server code that
-   we are writing, and it allows express to do a ton of work for us. app.js is essentially
-   the new version of our older server.js. This file is largely responsible for all the
-   initial setup that way other files can worry about the handling of requests, etc.
-*/
-
-// Import our libraries
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
